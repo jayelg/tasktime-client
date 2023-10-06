b5b164aae5dc2f6826c8f59db6988f703451466b
John Grant <john@raur.net>
2023-10-06 12:08:36 +1100

    initial commit

---
+++ src/pages/notFound.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="flex max-w-[980px] flex-col items-start gap-2">
+      <h1 className="font-sans text-3xl leading-tight tracking-tighter md:text-4xl">
+        404 Not Found.
+      </h1>
+    </div>
+  )
+}
+
+export default NotFound
